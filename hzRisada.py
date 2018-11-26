9c5a8909e70e9284cc579125de7ff310b2b4fd33
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-11-26 01:03:54 -0200

    Add files via upload
    Esse programa encrypta a mensagem para parecer uma risada de acordo com os valores dos caracteres em função da tabela ANSCII

---
+++ hzRisada.py
@@ -0,0 +1,18 @@
+def encrypt(In):
+	out = str();
+	for i in In:
+		out+=(ord(i.lower())-96)*'k'+'j'
+	return(out)
+
+def decrypt(In):
+	out = str();
+	In=In.split("j")
+	for i in In:
+		out+=chr(len(i)+96);
+	return(out)
+
+while(1):
+	a = input("Insira string:")
+	print(encrypt(a))
+	a = input("Insira string:")
+	print(decrypt(a))
