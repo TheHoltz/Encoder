b1a16c11ad56833774acd93cb4b530c7fccd9ec5
TheHoltz <38303017+TheHoltz@users.noreply.github.com>
2018-11-26 01:23:36 -0200

    O nº de k's depende da ocorrência no português.

---
+++ hzRisadaV2.py
@@ -0,0 +1,59 @@
+#Referencia: https://pt.wikipedia.org/wiki/Alfabeto_portugu%C3%AAs
+
+db = {
+"a":1,
+"e":2,
+"o":3,
+"s":4,
+"r":5,
+"i":6,
+"n":7,
+"d":8,
+"m":9,
+"u":10,
+"t":11,
+"c":12,
+"l":13,
+"p":14,
+"v":15,
+"g":16,
+"h":17,
+"q":18,
+"b":19,
+"f":20,
+"z":21,
+"j":22,
+"x":23,
+"k":24,
+"w":25,
+"y":26,
+" ":0,
+}
+
+def encrypt(string):
+    out = str();
+    for i in string:
+        if(i.lower() in db):
+            out+=(db[i]*'k'+'j');
+    return(out);
+
+def search(list,search_age):
+    for name,age in list.items():
+        if age == search_age:
+            return name
+
+def decrypt(In):
+    In = In.split("j")
+    out = str();
+    for i in In:
+	    for key,val in db.items():
+	        if val == len(i):
+	            out+=key;
+    return(out);
+
+
+while(1):
+	a = input("Insira string:")
+	print(encrypt(a))
+	a = input("Insira string:")
+	print(decrypt(a))
